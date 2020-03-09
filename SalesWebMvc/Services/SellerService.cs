1119dabdcd536e09ce9aec3a8525c6cedcd31390
Flavio <fblandim@gmail.com>
2020-03-09 00:38:51 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -43,5 +43,23 @@ namespace SalesWebMvc.Services
             _context.SaveChanges();
         }
 
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new DllNotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+        }
     }
 }
